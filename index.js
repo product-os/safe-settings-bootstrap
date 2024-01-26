84a0d5e237513b2a41bb70c55c2ba0af67bec2a1
Kyle Harding <kyle@balena.io>
2024-01-26 16:10:15 -0500

    Add support for exporting rulesets
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 require("dotenv").config();
 const { Probot, ProbotOctokit } = require("probot");
 const { throttling } = require("@octokit/plugin-throttling");
@@ -9,8 +10,9 @@ const path = require("path");
 const token = process.env.GITHUB_TOKEN;
 const org = process.env.ORG_NAME;
 const specificRepo = process.env.SPECIFIC_REPO; // Optionally set this to run on a single repo
-const reposDir = "./repos";
-const defaultSettingsPath = "./defaults.yml";
+const reposDir = path.join(process.cwd(), ".github", "repos");
+const orgSettingsPath = path.join(process.cwd(), ".github", "settings.yml");
+const packageSettingsPath = path.join(__dirname, 'assets', 'settings.yml');
 
 // Apply throttling plugin to Octokit
 const ThrottledOctokit = ProbotOctokit.plugin(throttling);
@@ -41,98 +43,8 @@ const probot = new Probot({
   }),
 });
 
-// Read the default settings from the YAML file
-const defaultSettings = yaml.parse(
-  fs.readFileSync(defaultSettingsPath, "utf8")
-);
-
-function flattenEnabledProperty(obj) {
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key) && obj[key] && typeof obj[key] === "object") {
-      if (Object.keys(obj[key]).length === 1 && "enabled" in obj[key]) {
-        // Flatten to just the 'enabled' value
-        obj[key] = obj[key].enabled;
-      } else {
-        // Recursively process nested objects
-        flattenEnabledProperty(obj[key]);
-      }
-    }
-  }
-}
-
-function removeSpecificProperties(obj, pathsToRemove) {
-  pathsToRemove.forEach((path) => {
-    const parts = path.split(".");
-    let current = obj;
-    for (let i = 0; i < parts.length - 1; i++) {
-      if (current[parts[i]]) {
-        current = current[parts[i]];
-      } else {
-        return; // Path not found, nothing to remove
-      }
-    }
-    delete current[parts[parts.length - 1]];
-  });
-}
-
-function arraysEqual(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-
-  const frequencyCounter1 = arr1.reduce((acc, val) => {
-    acc[val] = (acc[val] || 0) + 1;
-    return acc;
-  }, {});
-
-  const frequencyCounter2 = arr2.reduce((acc, val) => {
-    acc[val] = (acc[val] || 0) + 1;
-    return acc;
-  }, {});
-
-  for (let key in frequencyCounter1) {
-    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function compareObjects(template, actual) {
-  const diff = {};
-  let hasDiff = false;
-  for (const key in template) {
-    if (!template.hasOwnProperty(key)) continue;
-
-    // console.error(actual);
-
-    if (Array.isArray(template[key]) && actual[key]) {
-      // Compare arrays (lists) as unordered
-      if (!arraysEqual(template[key].sort(), actual[key].sort())) {
-        diff[key] = actual[key];
-        hasDiff = true;
-      }
-    } else if (
-      typeof template[key] === "object" &&
-      template[key] !== null &&
-      actual[key]
-    ) {
-      // Deep comparison for objects
-      const nestedDiff = compareObjects(template[key], actual[key]);
-      if (nestedDiff) {
-        diff[key] = nestedDiff;
-        hasDiff = true;
-      }
-    } else if (actual.hasOwnProperty(key) && template[key] !== actual[key]) {
-      diff[key] = actual[key];
-      hasDiff = true;
-    }
-  }
-  return hasDiff ? diff : null;
-}
-
 async function processRepository(octokit, repoName) {
-  let repoSettingsDiff;
-  let branchProtectionDiff = [];
+  let rulesets = [];
 
   // Fetch repository settings
   const repoData = await octokit.rest.repos.get({ owner: org, repo: repoName });
@@ -142,64 +54,115 @@ async function processRepository(octokit, repoName) {
     return;
   }
 
-  flattenEnabledProperty(repoData.data);
+  console.log(`Processing repository: ${repoName}`);
 
-  repoSettingsDiff = compareObjects(defaultSettings.repository, repoData.data);
+  try {
+    const protectionData = await octokit.rest.repos.getBranchProtection({
+      owner: org,
+      repo: repoName,
+      branch: repoData.data.default_branch,
+    });
 
-  for (const defaultBranch of defaultSettings.branches) {
-    try {
-      const protectionData = await octokit.rest.repos.getBranchProtection({
-        owner: org,
-        repo: repoName,
-        branch: defaultBranch.name,
-      });
+    const rulesetData = convertBranchProtectionToRuleset(protectionData.data);
 
-      // Remove specific properties
-      removeSpecificProperties(protectionData.data, [
-        "required_status_checks.checks",
-        "required_status_checks.url",
-        "required_status_checks.contexts_url",
-        "required_pull_request_reviews.url",
-        "required_pull_request_reviews.dismissal_restrictions.url",
-        "required_pull_request_reviews.dismissal_restrictions.teams_url",
-        "required_pull_request_reviews.dismissal_restrictions.users_url",
-        "enforce_admins.url",
-        // Add more paths to remove here as needed
-      ]);
+    // Check for 'Flowzone' in rules
+    const hasFlowzoneCheck = rulesetData.rules.some(
+      (rule) =>
+        rule.type === "required_status_checks" &&
+        rule.parameters.required_status_checks.some((check) =>
+          check.context.startsWith("Flowzone")
+        )
+    );
 
-      flattenEnabledProperty(protectionData);
+    if (hasFlowzoneCheck) {
+      // Load the YAML document
+      const fileContents = fs.readFileSync(orgSettingsPath, "utf8");
+      const doc = yaml.parseDocument(fileContents);
+    
+      // Get the rulesets array
+      const rulesets = doc.get('rulesets');
+    
+      // Find the Flowzone ruleset
+      const flowzoneRuleset = rulesets.items.find(
+        (ruleset) => ruleset.get('name') === "Flowzone"
+      );
+    
+      // Get the 'include' YAMLSeq from the repository_name conditions
+      const includeSeq = flowzoneRuleset.getIn(['conditions', 'repository_name', 'include']);
+    
+      // Convert the YAMLSeq to a JavaScript array and check if the repoName is included
+      if (!includeSeq.toJSON().includes(repoName)) {
 
-      const protectionOutput = compareObjects(
-        defaultBranch.protection,
-        protectionData.data
+        if (includeSeq.toJSON().length < 1) {
+          // replace the empty array with a bulleted list
+          flowzoneRuleset.deleteIn(['conditions', 'repository_name', 'include']);
+          flowzoneRuleset.addIn(['conditions', 'repository_name'], new yaml.Pair('include', [repoName]));
+        } else {
+          // Add the repository name to the include list
+          includeSeq.add(repoName);
+        }
+
+        // Write the updated document back to the file, preserving comments and formatting
+        fs.writeFileSync(orgSettingsPath, doc.toString(), "utf8");
+      }
+    }
+
+    // Exclude 'Flowzone' from the rulesetData rules before pushing it to rulesets
+    rulesetData.rules = rulesetData.rules.map((rule) => {
+      if (rule.type === "required_status_checks") {
+        rule.parameters.required_status_checks =
+          rule.parameters.required_status_checks.filter(
+            (check) => !check.context.startsWith("Flowzone")
+          );
+      }
+      return rule;
+    });
+
+    // Remove 'policy-bot' contexts and filter out rules without contexts
+    rulesetData.rules = rulesetData.rules
+      .map((rule) => {
+        if (rule.type === "required_status_checks") {
+          rule.parameters.required_status_checks =
+            rule.parameters.required_status_checks.filter(
+              (check) =>
+                !check.context.startsWith("policy-bot") &&
+                !check.context.startsWith("VersionBot") &&
+                !check.context.startsWith("ResinCI")
+            );
+        }
+        return rule;
+      })
+      .filter(
+        (rule) =>
+          !(
+            rule.type === "required_status_checks" &&
+            rule.parameters.required_status_checks.length === 0
+          )
       );
 
-      if (protectionOutput) {
-        branchProtectionDiff.push({
-          name: defaultBranch.name,
-          protection: protectionOutput,
-        });
-      }
-    } catch (error) {
-      // ignore error
+    // Only push to rulesets if there are rules remaining after filters
+    if (rulesetData.rules.length > 0) {
+      rulesets.push(rulesetData);
     }
+  } catch (error) {
+    // Skip repositories without rulesets
+    return;
   }
 
-  // Write YAML file if differences are found
-  if (repoSettingsDiff || branchProtectionDiff.length > 0) {
-    const jsonData = {};
-    if (repoSettingsDiff) {
-      jsonData.repository = repoSettingsDiff;
-    }
-    if (branchProtectionDiff.length > 0) {
-      jsonData.branches = branchProtectionDiff;
-    }
-    const yamlData = yaml.stringify(jsonData);
-    fs.writeFileSync(`./repos/${repoName}.yml`, yamlData, "utf8");
+  if (rulesets.length < 1) {
+    // Skip repositories without rulesets
+    return;
   }
+
+  const jsonData = {};
+  jsonData.rulesets = rulesets;
+
+  const yamlData = yaml.stringify(jsonData);
+  const filePath = path.join(reposDir, `${repoName}.yml`);
+  fs.writeFileSync(filePath, yamlData, "utf8");
 }
 
-async function main() {
+async function fetchRepoSettings() {
   const octokit = await probot.auth();
   if (!fs.existsSync(reposDir)) {
     fs.mkdirSync(reposDir);
@@ -226,4 +189,85 @@ async function main() {
   console.log("Repository settings processing complete.");
 }
 
+async function main() {
+  // replace the file at orgSettingsPath with the one bundled with this npm package
+  const yamlData = fs.readFileSync(packageSettingsPath, 'utf8');
+  fs.writeFileSync(orgSettingsPath, yamlData, "utf8");
+  await fetchRepoSettings();
+}
+
 main();
+
+function convertBranchProtectionToRuleset(branchProtection) {
+  const ruleset = {
+    name: "Default",
+    target: "branch",
+    enforcement: "active",
+    conditions: {
+      ref_name: {
+        exclude: [],
+        include: ["~DEFAULT_BRANCH"],
+      },
+    },
+    rules: [],
+    bypass_actors: [
+      {
+        actor_id: 1,
+        actor_type: "OrganizationAdmin",
+        bypass_mode: "always",
+      },
+      {
+        actor_id: 5,
+        actor_type: "RepositoryRole",
+        bypass_mode: "always",
+      },
+      {
+        actor_id: 291899,
+        actor_type: "Integration",
+        bypass_mode: "always",
+      },
+    ],
+  };
+
+  // Rule for required status checks
+  if (branchProtection.required_status_checks) {
+    const statusChecksRule = {
+      type: "required_status_checks",
+      parameters: {
+        // strict_required_status_checks_policy:
+        //   branchProtection.required_status_checks.strict,
+        strict_required_status_checks_policy: true,
+        required_status_checks:
+          branchProtection.required_status_checks.checks.map((check) => ({
+            context: check.context,
+            integration_id: check.app_id,
+          })),
+      },
+    };
+    ruleset.rules.push(statusChecksRule);
+  }
+
+  // // Rule for pull request reviews
+  // if (branchProtection.required_pull_request_reviews) {
+  //   const prReviewsRule = {
+  //     type: "pull_request",
+  //     parameters: {
+  //       required_approving_review_count: 0,
+  //       dismiss_stale_reviews_on_push:
+  //         branchProtection.required_pull_request_reviews.dismiss_stale_reviews,
+  //       require_code_owner_review:
+  //         branchProtection.required_pull_request_reviews
+  //           .require_code_owner_reviews,
+  //       require_last_push_approval:
+  //         branchProtection.required_pull_request_reviews
+  //           .require_last_push_approval,
+  //       required_review_thread_resolution: false, // Set as needed
+  //     },
+  //   };
+  //   ruleset.rules.push(prReviewsRule);
+  // }
+
+  // Add other rules based on branchProtection object as needed
+
+  return ruleset;
+}
